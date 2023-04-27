8258b728726467bf93196d3527cea2927079181c
jeppeSiegfredsen <10438303@ucn.dk>
2023-04-27 10:14:39 +0200

    n

---
+++ SemesterProjekt3Web/Models/MovieCopy.cs
@@ -0,0 +1,13 @@
+﻿namespace SemesterProjekt3Api.Model
+{
+    public class MovieCopy
+    {
+
+        public int Id { get; set; }
+        public string Language { get; set; }
+        public bool Is3D { get; set; }
+        public double Price { get; set; }
+        public MovieInfo MovieType { get; set; }
+
+    }
+}
---
+++ SemesterProjekt3Web/Models/ShowRoom.cs
@@ -0,0 +1,11 @@
+﻿namespace SemesterProjekt3Api.Model
+{
+    public class ShowRoom
+    {
+
+        public int RoomNumber { get; set; }
+        public int Capacity { get; set; }
+        public List<Seat> Seats { get; set; }
+
+    }
+}
