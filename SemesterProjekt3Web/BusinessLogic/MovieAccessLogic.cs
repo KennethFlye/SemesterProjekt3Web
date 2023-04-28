b966fed7fec9d871b043c3986db8c7ad59791698
jeppeSiegfredsen <10438303@ucn.dk>
2023-04-28 08:54:47 +0200

    api start

---
+++ SemesterProjekt3Web/BusinessLogic/MovieAccessLogic.cs
@@ -0,0 +1,46 @@
+﻿using SemesterProjekt3Api.Model;
+using SemesterProjekt3Web.Access;
+
+
+namespace SemesterProjekt3Web.BusinessLogic
+{
+    public class MovieAccessLogic
+    {
+
+        private readonly MovieAccess api;
+
+        public MovieAccessLogic()
+        {
+            api = new MovieAccess();
+        }
+
+        public async Task<List<MovieInfo>> GetMovies()
+        {
+            List<MovieInfo> movies;
+
+            try
+            {
+                movies = await api.GetMovies();
+            }
+            catch (Exception)
+            {
+                movies = null;
+            }
+            return movies;
+        }
+        public async Task<List<Showing>> GetShowingsByMovieID(int id)
+        {
+            List<Showing> showings;
+
+            try
+            {
+                showings = await api.GetShowingsByMovieID(id);
+            }
+            catch (Exception)
+            {
+                showings = null;
+            }
+            return showings;
+        }
+    }
+}
