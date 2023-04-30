e5ea0766bf059dc821f0e1ca96e2e2e16042f968
oskarbucn <10434661@ucn.dk>
2023-04-30 17:10:23 +0200

    Added checking for regex

--- SemesterProjekt3Web/wwwroot/js/site.js
+++ SemesterProjekt3Web/wwwroot/js/site.js
@@ -1,4 +1,3 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿var namePattern = /^[a-zA-Z\s]+$/
+var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+var phoneNoPattern = /^(\+?\d{1,3}[- ]?)?\d{6,14}$/
