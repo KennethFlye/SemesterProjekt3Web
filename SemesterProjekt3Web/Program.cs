ee2d20102fd608375e597dccee3859a0b2a826f6
oskarbucn <10434661@ucn.dk>
2023-05-01 17:35:19 +0200

    Revert "Test stuff"
    This reverts commit f469abafff6825c4ca7dcd7e833c66077663eb25.

--- SemesterProjekt3Web/Program.cs
+++ SemesterProjekt3Web/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Showings}/{id?}");
+    pattern: "{controller=Booking}/{action=CustomerInfo}/{id?}");
 
 app.Run();
