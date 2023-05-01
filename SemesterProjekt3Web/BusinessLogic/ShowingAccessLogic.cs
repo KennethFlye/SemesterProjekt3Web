f469abafff6825c4ca7dcd7e833c66077663eb25
oskarbucn <10434661@ucn.dk>
2023-05-01 17:34:40 +0200

    Test stuff

--- SemesterProjekt3Web/BusinessLogic/ShowingAccessLogic.cs
+++ SemesterProjekt3Web/BusinessLogic/ShowingAccessLogic.cs
@@ -1,29 +1,29 @@
-﻿using SemesterProjekt3Web.ApiAccess;
-using SemesterProjekt3Web.Models;
+﻿//using SemesterProjekt3Web.ApiAccess;
+//using SemesterProjekt3Web.Models;
 
-namespace SemesterProjekt3Web.BusinessLogic
-{
-    public class ShowingAccessLogic
-    {
-        public ShowingAccessLogic()
-        {
-            var api = new ShowingAccess();
-        }
-        public async Task<Showing> GetShowingById(int id)
-        {
-             Showing show; 
+//namespace SemesterProjekt3Web.BusinessLogic
+//{
+//    public class ShowingAccessLogic
+//    {
+//        public ShowingAccessLogic()
+//        {
+//            var api = new ShowingAccess();
+//        }
+//        public async Task<Showing> GetShowingById(int id)
+//        {
+//             Showing show; 
 
-            try
-            {
-                show = await api.GetShowingById(id);
-            }
-            catch (Exception)
-            {
-                show = null;
-            }
-            return show;
-        }
+//            try
+//            {
+//                show = await api.GetShowingById(id);
+//            }
+//            catch (Exception)
+//            {
+//                show = null;
+//            }
+//            return show;
+//        }
 
 
-    }
-}
+//    }
+//}
--- SemesterProjekt3Web/Controllers/HomeController.cs
+++ SemesterProjekt3Web/Controllers/HomeController.cs
@@ -40,8 +40,36 @@ namespace SemesterProjekt3Web.Controllers
 
         public ActionResult Showings(int id)
         {
-            Task<IEnumerable<Showing>> mInfo = _movieAL.GetShowingsByMovieID(id);
-            var nInfo = mInfo.GetAwaiter().GetResult();
+            //Task<IEnumerable<Showing>> mInfo = _movieAL.GetShowingsByMovieID(id);
+            //var nInfo = mInfo.GetAwaiter().GetResult();
+            List<Showing> nInfo = new List<Showing>();
+
+            Showing showing1 = new Showing();
+            showing1.Id = 1;
+            showing1.startTime = DateTime.Now;
+            showing1.IsKidFriendly = false;
+            showing1.ShowRoom = new ShowRoom();
+            showing1.MovieCopy = new MovieCopy();
+
+            showing1.ShowRoom.RoomNumber = 4;
+            showing1.ShowRoom.Capacity = 32;
+            showing1.ShowRoom.Seats = new List<Seat>();
+
+            showing1.MovieCopy.Id = 2;
+            showing1.MovieCopy.Language = "Engelsk";
+            showing1.MovieCopy.Is3D = false;
+            showing1.MovieCopy.Price = 100;
+            showing1.MovieCopy.MovieType = new MovieInfo();
+
+            showing1.MovieCopy.MovieType.Id = 3;
+            showing1.MovieCopy.MovieType.Title = "Shrek 2";
+            showing1.MovieCopy.MovieType.Length = 119;
+            showing1.MovieCopy.MovieType.Genre = "Adventure/Family";
+            showing1.MovieCopy.MovieType.PgRating = "+7 år";
+            showing1.MovieCopy.MovieType.PremiereDate = new DateTime(2001, 07, 09);
+
+            nInfo.Add(showing1);
+
             return View(nInfo);
         }
 
