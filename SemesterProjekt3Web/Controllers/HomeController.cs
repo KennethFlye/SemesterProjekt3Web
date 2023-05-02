745396ffa304b2c060da00b706d19f755c47e8e6
oskarbucn <10434661@ucn.dk>
2023-05-02 10:10:31 +0200

    yes

--- SemesterProjekt3Web/Controllers/HomeController.cs
+++ SemesterProjekt3Web/Controllers/HomeController.cs
@@ -23,6 +23,8 @@ namespace SemesterProjekt3Web.Controllers
             return View();
         }
 
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
