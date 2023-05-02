b2cad82f8dd13a20c2c18be2447fb76fa3d08af2
oskarbucn <10434661@ucn.dk>
2023-05-02 09:21:39 +0200

    Update Program.cs

--- SemesterProjekt3Web/Program.cs
+++ SemesterProjekt3Web/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Showings}/{id?}");
+    pattern: "{controller=Home}/{action=Movies}/{id?}");
 
 app.Run();
