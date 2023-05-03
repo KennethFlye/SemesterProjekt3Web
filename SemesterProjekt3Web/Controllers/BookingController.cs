5e0930f54fc72a351503cee0997393b2982efb2e
KennethFlye <10434642@ucn.dk>
2023-05-03 13:46:32 +0200

    Alt undtagen godkend implementeret

--- SemesterProjekt3Web/Controllers/BookingController.cs
+++ SemesterProjekt3Web/Controllers/BookingController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SemesterProjekt3Api.Model;
 using SemesterProjekt3Web.Models;
 using System.Diagnostics;
 
@@ -20,6 +22,20 @@ namespace SemesterProjekt3Web.Controllers
         public IActionResult CustomerInfo()
         {
             return View();
+        }
+
+        public IActionResult Seats()
+        {
+            HttpClient client = new HttpClient();
+
+            var respTask = client.GetAsync("https://localhost:7155/api/showings/3");
+            respTask.Wait();
+            StreamReader sr = new(respTask.Result.Content.ReadAsStream());
+            string stringResult = sr.ReadToEnd();
+            sr.Close();
+            Showing foundShowing = JsonConvert.DeserializeObject<Showing>(stringResult);
+
+            return View(foundShowing);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- SemesterProjekt3Web/Models/ShowRoom.cs
+++ SemesterProjekt3Web/Models/ShowRoom.cs
@@ -7,5 +7,19 @@
         public int Capacity { get; set; }
         public List<Seat> Seats { get; set; }
 
+        public int GetAmountOfRows()
+        {
+            int amountOfRows = 0;
+            foreach(Seat seat in Seats)
+            {
+                if (seat.RowNumber > amountOfRows)
+                {
+                    amountOfRows = seat.RowNumber;
+                }
+            }
+
+            return amountOfRows;
+        }
+
     }
 }
