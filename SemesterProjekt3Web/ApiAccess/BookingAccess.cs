73d33cd1d0dcc2c9f8bd1d81cb7c1d4efbc82888
jeppeSiegfredsen <10438303@ucn.dk>
2023-05-03 10:53:02 +0200

    api acs update

--- SemesterProjekt3Web/ApiAccess/BookingAccess.cs
+++ SemesterProjekt3Web/ApiAccess/BookingAccess.cs
@@ -17,11 +17,9 @@ namespace SemesterProjekt3Web.ApiAccess
 
 
 
-        public async Task<bool> AddBooking(Booking item)
+        public async Task<bool> AddBooking(Booking res)
         {
             bool savedOk = false;
-
-
             var uri = new Uri(baseUrl);
 
             try
@@ -29,80 +27,83 @@ namespace SemesterProjekt3Web.ApiAccess
                 var response = await client.GetAsync(uri);
                 if (response.IsSuccessStatusCode)
                 {
-                    var json = JsonConvert.SerializeObject(item);
+                    var json = JsonConvert.SerializeObject(res);
                     var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-                    var serviceResponse = await _lineServiceConnection.CallServicePost(content);
-                    if (serviceResponse != null && serviceResponse.IsSuccessStatusCode)
+                    var postResponse = await client.PostAsync(uri, content);
+                    if (postResponse != null && postResponse.IsSuccessStatusCode)
                     {
                         savedOk = true;
                     }
                 }
-                    catch
+
+
+            }
+            catch
             {
                 savedOk = false;
             }
+            return savedOk;
         }
-    
-        }
-
-             
-    public async Task<Booking> GetBookingById(int id)
-    {
-        Booking book;
 
 
-        string url = baseUrl + $"/{id}";
-        var uri = new Uri(string.Format(url));
 
-
-        try
+        public async Task<Booking> GetBookingById(int id)
         {
-            var response = await client.GetAsync(uri);
-            if (response.IsSuccessStatusCode)
+            Booking book;
+
+
+            string url = baseUrl + $"/{id}";
+            var uri = new Uri(string.Format(url));
+
+
+            try
             {
-                var content = await response.Content.ReadAsStringAsync();
-                book = JsonConvert.DeserializeObject<Booking>(content);
+                var response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    book = JsonConvert.DeserializeObject<Booking>(content);
+                }
+                else
+                {
+                    book = null;
+                }
             }
-            else
+            catch
             {
-                book = null;
+                throw;
             }
+            return book;
         }
-        catch
+        public async Task<IEnumerable<Seat>> GetSeatsByBooking()
         {
-            throw;
+            List<Seat> seats;
+
+
+            string url = baseUrl + $"/seats";
+            var uri = new Uri(string.Format(url));
+
+
+            try
+            {
+                var response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    seats = JsonConvert.DeserializeObject<List<Seat>>(content);
+                }
+                else
+                {
+                    seats = null;
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            return seats;
         }
-        return book;
+
     }
-    public async Task<IEnumerable<Seat>> GetSeatsByBooking()
-    {
-        List<Seat> seats;
-
-
-        string url = baseUrl + $"/seats";
-        var uri = new Uri(string.Format(url));
-
-
-        try
-        {
-            var response = await client.GetAsync(uri);
-            if (response.IsSuccessStatusCode)
-            {
-                var content = await response.Content.ReadAsStringAsync();
-                seats = JsonConvert.DeserializeObject<List<Seat>>(content);
-            }
-            else
-            {
-                seats = null;
-            }
-        }
-        catch
-        {
-            throw;
-        }
-        return seats;
-    }
-
 }
-}
+
